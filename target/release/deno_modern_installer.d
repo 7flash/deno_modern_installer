34fb6ec023a49f280f575dcd05d44fa9cd94a825
Przemysław Orłowski <przemek.orlowski@outlook.com>
2023-01-24 19:48:47 +0100

    Add prebuilt libraries and enable it in gitignore

---
+++ target/release/deno_modern_installer.d
@@ -0,0 +1,1 @@
+D:\Projects\deno\deno_modern_installer\target\release\deno_modern_installer.dll: D:\Projects\deno\deno_modern_installer\src\lib.rs
---
+++ target/release/libdeno_modern_installer.d
@@ -0,0 +1,1 @@
+/mnt/d/Projects/deno/deno_modern_installer/target/release/libdeno_modern_installer.so: /mnt/d/Projects/deno/deno_modern_installer/src/lib.rs
