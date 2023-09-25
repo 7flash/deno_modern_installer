0fe1f2408acc0b744c613e5f8a3cd7058870300a
Igor Berlenko <sevenflash@yandex.ru>
2023-09-25 21:38:09 +0800

    sep25 - third

--- target/release/libdeno_modern_installer.d
+++ target/release/libdeno_modern_installer.d
@@ -1,1 +1,1 @@
-/Users/macbook/Documents/galaxy-polkadot/desktop/installer/target/release/libdeno_modern_installer.dylib: /Users/macbook/Documents/galaxy-polkadot/desktop/installer/src/lib.rs
+/mnt/d/Projects/deno/deno_modern_installer/target/release/libdeno_modern_installer.so: /mnt/d/Projects/deno/deno_modern_installer/src/lib.rs
